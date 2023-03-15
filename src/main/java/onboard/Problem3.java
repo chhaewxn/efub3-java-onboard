0d314cc065f28b9f33efab6abe6c5977a19d7342
chhaewxn <chaewon1019@ewhain.net>
2023-03-15 14:15:41 +0900

    ✨ feat: solve Problem3

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -1,11 +1,28 @@
 package onboard;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
         List<Integer> answer = new ArrayList<>();
+        int sum = 0;
+
+        Collections.sort(numbers);
+
+        for(int i=0; i<5; i++){
+            sum += numbers.get(i);
+        }
+        int average = sum / 5;
+
+        int mid;
+        mid = numbers.get(numbers.size() / 2);
+
+        answer.add(average);
+        answer.add(mid);
+
         return answer;
     }
 }
--- src/main/java/onboard/Problem5.java
+++ src/main/java/onboard/Problem5.java
@@ -1,8 +1,25 @@
 package onboard;
 
+import java.util.Arrays;
+
 public class Problem5 {
     public static Integer solution(Integer N, Integer K, Integer[] values){
         Integer answer = 0;
+
+        Integer[] coin = new Integer[K+1];
+        Arrays.fill(coin, 100001);
+
+        coin[1] = 1;
+
+        for(int i=2; i<=K; i++) {
+            for(int j=0; j<N; j++) {
+                if(i - values[j] >= 1) {
+                    coin[i] = Math.min(coin[i], coin[i-values[j]]+1);
+                }
+            }
+        }
+
+        answer = coin[K];
         return answer;
     }
 }
